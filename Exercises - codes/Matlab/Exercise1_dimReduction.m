941e54dfddf165b2d3ff22601da045f09d1d0712
AdrienTaylor <{10559960}+{AdrienTaylor}@users.noreply.github.com>
2022-07-01 17:28:58 +0200

    Ex 1

--- Exercises - codes/Matlab/Exercise1_dimReduction.m
+++ Exercises - codes/Matlab/Exercise1_dimReduction.m
@@ -1,20 +1,14 @@
 clear all; clc;
-% This code corresponds to exercise 1 of the document
+% This code corresponds to exercise 1.13 of the document
 % https://github.com/PerformanceEstimation/Learning-Performance-Estimation/blob/main/Exercises/Course.pdf
-%
-% This code must be completed for computing the worst-case ratio 
-% ||x_{k+1} - x_*||^2 / ||x_k - x_*||^2 (note: we use k=0 without loss of
-% generality for readability purposes!)
-%
-% Complete the code below accordingly:
-
 
 % Initialize an empty PEP
 P = pep();
 
 % Set up the objective function (fix some parameter values)
-paramf1.mu = .1;        % Strong convexity parameter
+paramf1.mu = 0;         % Strong convexity parameter
 paramf1.L  = 1;         % Smoothness parameter
+N          = 5;         % Number of iterations under consideration
 
 f = P.DeclareFunction('SmoothStronglyConvex',paramf1);
 
@@ -24,15 +18,20 @@ x0      = P.StartingPoint();            % x0 is some starting point
 
 % Algorithm: gradient descent
 gamma = 1;              % stepsize
-g0 = f.gradient(x0);
-x1 = x0 - gamma * g0;
+x = x0;
+for i = 1:N
+    g = f.gradient(x);
+    x = x - gamma * g;
+end
 
 % COMPLETE the following lines
-P.InitialCondition( );          % Initial condition ||x0-xs||^2<= 1
-P.PerformanceMetric( );         % Performance metric ||x1-xs||^2
+P.InitialCondition( (x0-xs)^2 <= 1);    % Initial condition ||x0-xs||^2<= 1
+P.PerformanceMetric( (x-xs)^2 );        % Performance metric ||x1-xs||^2
 
 % (5) Solve the PEP
+P.TraceHeuristic(1); % tries to find a low-dimensional worst-case instance
 P.solve()
 
 % (6) Evaluate the output
-double((x1-xs)^2)   % worst-case distance
+double((x-xs)^2)    % worst-case distance after N iteration (1==no progress)
+double(g^2)         % gradient used in the last gradient step
